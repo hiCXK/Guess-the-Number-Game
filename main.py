00dc1de9a328d4339b8ac44160a4e71be00fb81f
chaitanya <hicsk24@gmail.com>
2023-04-13 23:18:30 +0530

    edited and added some more parts of the code

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from random import *
 a=None
+
+c=1
+
+
 name=input("Please enter your name: ")
 print("Hello " + name + "!\nI have chosen a number[1,100]...can you guess it?")
-x=int(input("Enter your number here: "))
-y=randint(1,100)
+
 def getnum():
     x=int(input("Try again: "))
     compare(x,y)
+
+def getnum2():
+    global y
+    y=randint(1,100)
+    x=int(input("Enter your number: "))
+    compare(x,y)
 def compare(x,y):
     if (y==x):
-        print("YES! You got that right.")
+        print("YES! You got that right!")
         a=input("Play again? Y/N: ") 
         if a=="Y":
+            print("I have again chosen a number[1,100]. Find it!")
             y=randint(1,100)
-            getnum()
+            getnum2()
         if a=="N":
             print("Have a great day " + name + "! Until next time...")
     elif(y>x):
-        print("low")
+        print("It is low")
         getnum()
     else:
-        print("high")
+        print("It is high")
         getnum()
         
            
-getnum() 
+getnum2() 
